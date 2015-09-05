ffce0bd26352d3a11410b9087413e746dc9d11e2
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-09-06 08:41:44 +0900

    commit

--- blinkTestApp/ViewController.swift
+++ blinkTestApp/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blinkView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIView.animateWithDuration(1.0, delay: 0.0,
+            options: UIViewAnimationOptions.Repeat, animations: { () -> Void in
+            self.blinkView.alpha = 0.0
+            }, completion: nil)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pressButton(sender: AnyObject) {
+        
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.animateWithDuration(0.001, animations: {
+            self.blinkView.alpha = 1.0
+            })
     }
 
 
